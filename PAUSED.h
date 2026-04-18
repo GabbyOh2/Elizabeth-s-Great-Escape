b904eb1499bf07f8ca28b732f2c5a1807a6751c2
Gabrielle Oh <156555529+GabbyOh2@users.noreply.github.com>
2026-04-18 01:33:14 -0400

    Add files via upload

---
+++ PAUSED.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(PAUSED)
+
+//======================================================================
+//
+//	PAUSED, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2025-12-07, 02:09:11
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PAUSED_H
+#define GRIT_PAUSED_H
+
+#define PAUSEDBitmapLen 38400
+extern const unsigned short PAUSEDBitmap[19200];
+
+#define PAUSEDPalLen 512
+extern const unsigned short PAUSEDPal[256];
+
+#endif // GRIT_PAUSED_H
+
+//}}BLOCK(PAUSED)
---
+++ parallax1.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(parallax1)
+
+//======================================================================
+//
+//	parallax1, 256x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 495 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 15840 + 2048 = 18400
+//
+//	Time-stamp: 2025-11-26, 22:10:39
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PARALLAX1_H
+#define GRIT_PARALLAX1_H
+
+#define parallax1TilesLen 15840
+extern const unsigned short parallax1Tiles[7920];
+
+#define parallax1MapLen 2048
+extern const unsigned short parallax1Map[1024];
+
+#define parallax1PalLen 512
+extern const unsigned short parallax1Pal[256];
+
+#endif // GRIT_PARALLAX1_H
+
+//}}BLOCK(parallax1)
