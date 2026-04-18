9dd85f214180c9c23dccd413ca5476799db48405
Gabrielle Oh <156555529+GabbyOh2@users.noreply.github.com>
2026-04-18 01:37:30 -0400

    Add files via upload

---
+++ WIN.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(WIN)
+
+//======================================================================
+//
+//	WIN, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2025-12-10, 14:11:25
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WIN_H
+#define GRIT_WIN_H
+
+#define WINBitmapLen 38400
+extern const unsigned short WINBitmap[19200];
+
+#define WINPalLen 512
+extern const unsigned short WINPal[256];
+
+#endif // GRIT_WIN_H
+
+//}}BLOCK(WIN)
---
+++ whoosh.h
@@ -0,0 +1,5 @@
+// whoosh sound made by wav2c
+
+extern const unsigned int whoosh_sampleRate;
+extern const unsigned int whoosh_length;
+extern const signed char whoosh_data[];
